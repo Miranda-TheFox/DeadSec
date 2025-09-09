c1136b6e746fcc5a63303849a559a78d276ccfde
Miranda-TheFox <davivazprofissional@gmail.com>
2025-09-09 08:42:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function teste(){
+
+var usuario = document.getElementById("usuario") .value;
+var senha = document. getElementById("senha") .value;
+
+if(usuario == "Davivaz" && senha == "d4v14444") window. location.href = "continuaçao.html";
+else alert("Usuario ou Senha incorretos.")
+
+}
